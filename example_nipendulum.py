ca64def3ddd06988a2def15d48837bc250437e72
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2019-07-16 15:30:52 +0200

    Added non inertial pendulum

--- example_nipendulum.py
+++ example_nipendulum.py
@@ -7,23 +7,27 @@ import matplotlib.animation as animation
 
 ## Set-up your problem
 steps = 1000
-ts = np.linspace(0, 10, steps) # Simulation time
-yinit = (0, 3) # Initial condition (th_0, w_0)
-l = 1.5
-f = lambda state, t : pendulum(state, t, l = l)
+ts = np.linspace(-5, 10, steps) # Simulation time
+yinit = (0.1, 0) # Initial condition (th_0, w_0)
+g = 9.8
+l = 1
+
+pos_x = lambda t : np.arctan(5*t)
+pos_y = lambda t : 0*t
+f = lambda state, t : ni_pendulum(state, t, pos_x, pos_y, is_acceleration = False, g = g, l = l, d = 1) # Dynamical equation as a function of (state, t)
 
 ## Solve it
 sol = odeint(f, yinit, ts)
 
 ## Extract each coordinate
-x = l*np.sin(sol[:, 0])
-y = -l*np.cos(sol[:, 0])
-x_pivot = np.zeros(len(x))
-y_pivot = np.zeros(len(y))
+x_pivot = pos_x(ts)
+y_pivot = pos_y(ts)
+x = x_pivot + l*np.sin(sol[:, 0])
+y = y_pivot - l*np.cos(sol[:, 0])
 
 ## Animate results
 fig = plt.figure()
-ax = fig.add_subplot(111, aspect='equal', xlim=(-1.5*l, 1.5*l), ylim=(-1.5*l, 1.5*l))
+ax = fig.add_subplot(111, aspect='equal', xlim=(-3, 3), ylim=(-3, 3))
 ax.grid()
 
 line, = ax.plot([], [], 'o-', lw=1)
@@ -49,5 +53,8 @@ ani = animation.FuncAnimation(fig, animate, np.arange(1, len(ts)),
                               interval=2, blit=True, init_func=init)
 
 
-plt.legend()
+# Set up formatting for the movie files
+#Writer = animation.writers['ffmpeg']
+#writer = Writer(fps=100, metadata=dict(artist='Me'), bitrate=1800)
+#ani.save('im.mp4', writer = writer)
 plt.show()
--- pendulum/models.py
+++ pendulum/models.py
@@ -6,16 +6,48 @@ def pendulum(state, t=0, l=1, g=9.8, d=0):
     Parameters:
     state: the state (angle, angular speed)
     t: the time
-    l: the pendulum's lenght
+    l: the pendulum's length
     g: the local acceleration of gravity
     d: damping constant
 
     Returns:
     dydt: the time derivative
 
-   """
+    """
     th, w = state
     dydt = [w,
             -g/l * np.sin(th) - d * w]
+
+    return dydt
+
+def ni_pendulum(state, t, pivot_x, pivot_y, is_acceleration=False, l=1.0, g=9.8, d=0.0, h=1e-4):
+    """Returns the dynamical equation of a non inertial pendulum
+
+    Parameters:
+    state: the state (angle, angular speed)
+    t: the time
+    l: the pendulum's lenght
+    g: the local acceleration of gravity
+    pivot_x: the horizontal position of the pivot
+    pivot_y: the vertical position of the pivot
+    is_acceleration: set to True to input pivot accelerations instead of positions
+    h: numerical step for computing numerical derivatives
+
+    Returns:
+    dydt: the time derivative
+
+    """
+    if is_acceleration:
+        accel_x = lambda t : pivot_x(t)
+        accel_y = lambda t : pivot_y(t)
+    else: # Compute the acceleration numerically
+        speed_x = lambda t : (pivot_x(t + h) - pivot_x(t))/h
+        speed_y = lambda t : (pivot_y(t + h) - pivot_y(t))/h
+        accel_x = lambda t : (speed_x(t + h) - speed_x(t))/h
+        accel_y = lambda t : (speed_y(t + h) - speed_y(t))/h
+
+    th, w = state
+    dydt = [w,
+            -g/l * np.sin(th)  - d * w - accel_x(t) * np.cos(th) - accel_y(t) * np.sin(th)]
 
     return dydt
