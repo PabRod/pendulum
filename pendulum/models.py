8e667cdff633af79afd5578a62a48d401b59d73c
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2019-07-16 14:49:36 +0200

    Added damping constant to simple pendulum

--- pendulum/models.py
+++ pendulum/models.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def pendulum(state, t=0, l=1, g=9.8):
+def pendulum(state, t=0, l=1, g=9.8, d=0):
     """Returns the dynamical equation of a simple pendulum
 
     Parameters:
@@ -8,6 +8,7 @@ def pendulum(state, t=0, l=1, g=9.8):
     t: the time
     l: the pendulum's lenght
     g: the local acceleration of gravity
+    d: damping constant
 
     Returns:
     dydt: the time derivative
@@ -15,6 +16,6 @@ def pendulum(state, t=0, l=1, g=9.8):
    """
     th, w = state
     dydt = [w,
-            -g/l * np.sin(th)]
+            -g/l * np.sin(th) - d * w]
 
     return dydt
--- pendulum/tests/test_models.py
+++ pendulum/tests/test_models.py
@@ -1,16 +1,58 @@
 from pendulum.models import *
-import pytest
+from scipy.integrate import odeint
 import numpy as np
+import pytest
 
 @pytest.mark.parametrize("input, exp_output", [
     ((0, 0), (0, 0)), # Stable equilibrium
     ((np.pi, 0), (0, 0))  # Unstable equilibrium
 ])
 def test_pendulum(input, exp_output):
-
+    ''' Test the equilibrium solutions
+    '''
     tol = 1e-8
 
     df = pendulum(input)
 
     assert(df == pytest.approx(exp_output, tol)), \
         'pendulum is not behaving as expected'
+
+def test_damped_pendulum():
+    ''' Test the long-term solution of a damped pendulum
+    '''
+    tol = 1e-8
+
+    ## Set the problem
+    ts = np.linspace(0, 100, 100) # Simulation time
+    yinit = (0, 1) # Initial condition (th_0, w_0)
+
+    f = lambda state, t : pendulum(state, t, d = 2)
+
+    ## Solve it
+    sol = odeint(f, yinit, ts)
+
+    last_theta = sol[-1, 0]
+    last_w = sol[-1, 1]
+
+    assert(last_theta == pytest.approx(0.0, tol))
+    assert(last_w == pytest.approx(0.0, tol))
+
+def test_undamped_pendulum():
+    ''' Test the long-term solution of a damped pendulum
+    '''
+    tol = 1e-8
+
+    ## Set the problem
+    ts = np.linspace(0, 100, 100) # Simulation time
+    yinit = (0, 1) # Initial condition (th_0, w_0)
+
+    f = lambda state, t : pendulum(state, t, d = 0)
+
+    ## Solve it
+    sol = odeint(f, yinit, ts)
+
+    last_theta = sol[-1, 0]
+    last_w = sol[-1, 1]
+
+    assert(last_theta != pytest.approx(0.0, tol))
+    assert(last_w != pytest.approx(0.0, tol))
