1386ee19fbbb2ba2c29908b43cf6a3380e482f23
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2019-08-19 14:26:58 +0200

    Input control relocated
    Now it happens just before the parameter is being used

--- pendulum/models.py
+++ pendulum/models.py
@@ -20,6 +20,16 @@ def dpendulum(state, t=0, pivot_x=0.0, pivot_y=0.0, is_acceleration=False, l=1.0
 
     """
 
+    ## Avoid wrong inputs
+    if (l <= 0.0): # Negative or zero lengths don't make sense
+        raise ValueError('Wrong pendulum length (l). Expected positive float')
+
+    if (d < 0.0): # A negative damping constant doesn't make sense
+        raise ValueError('Wrong damping constant (d). Expected zero or positive float')
+
+    if (h <= 0.0): # The numerical step for differentiation has to be positive
+        raise ValueError('Wrong numerical step (h). Expected a positive float')
+
     ## Flexible input interpretation
     accel_x, accel_y = _format_accelerations(pivot_x, pivot_y, is_acceleration, h)
 
@@ -51,17 +61,8 @@ def pendulum(yinit, ts, pivot_x=0.0, pivot_y=0.0, is_acceleration=False, l=1.0, 
     """
 
     ## Avoid wrong inputs
-    if (l <= 0.0): # Negative or zero lengths don't make sense
-        raise ValueError('Wrong pendulum length (l). Expected positive float')
-
-    if (d < 0.0): # A negative damping constant doesn't make sense
-        raise ValueError('Wrong damping constant (d). Expected zero or positive float')
-
     if (len(yinit) != 2): # The initial conditions are (th_0, w_0). No more, and no less
         raise ValueError('Wrong initial condition (yinit). Expected 2-elements vector')
-
-    if (h <= 0.0): # The numerical step for differentiation has to be positive
-        raise ValueError('Wrong numerical step (h). Expected a positive float')
 
     ## Set the problem
     f = lambda state, t : dpendulum(state, t, pivot_x, pivot_y, is_acceleration, l, g, d, h)
@@ -89,6 +90,13 @@ def ddouble_pendulum(state, t=0, pivot_x=0.0, pivot_y=0.0, is_acceleration=False
     :returns: the time derivative (dydt)
 
     """
+
+    ## Avoid wrong inputs
+    if (np.min(m) <= 0.0) or (len(m) != 2): # Negative or zero masses don't make sense
+        raise ValueError('Wrong pendulum masses (m). Expected 2 positive floats')
+
+    if (np.min(l) <= 0.0) or (len(l) != 2): # Negative or zero lengths don't make sense
+        raise ValueError('Wrong pendulum lengths (l). Expected 2 positive floats')
 
     ## Flexible input interpretation
     accel_x, accel_y = _format_accelerations(pivot_x, pivot_y, is_acceleration, h)
@@ -142,12 +150,6 @@ def double_pendulum(yinit, ts, pivot_x=0.0, pivot_y=0.0, is_acceleration=False, 
     """
 
     ## Avoid wrong inputs
-    if (np.min(m) <= 0.0) or (len(m) != 2): # Negative or zero masses don't make sense
-        raise ValueError('Wrong pendulum masses (m). Expected 2 positive floats')
-
-    if (np.min(l) <= 0.0) or (len(l) != 2): # Negative or zero lengths don't make sense
-        raise ValueError('Wrong pendulum lengths (l). Expected 2 positive floats')
-
     if (len(yinit) != 4): # The initial conditions are (th_0, w_0, th_1, w_!). No more, and no less
         raise ValueError('Wrong initial condition (yinit). Expected 4-elements vector')
 
